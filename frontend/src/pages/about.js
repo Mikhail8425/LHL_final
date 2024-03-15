c871435fdda29a64efc8839a43613de81f1911e7
Eric <ericbradshaw98@gmail.com>
2024-03-15 10:55:06 -0400

    added routes for pages

---
+++ frontend/src/pages/about.js
@@ -0,0 +1,15 @@
+// Filename - pages/about.js
+ 
+import React from "react";
+ 
+const About = () => {
+    return (
+        <div>
+            <h1>
+                stock all about stocks.
+            </h1>
+        </div>
+    );
+};
+ 
+export default About;
