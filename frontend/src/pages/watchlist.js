610c38cd0a7851cc5c5a78ad362e7f7d11600a37
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-20 12:57:09 -0400

    displaying all keys and values of the response object

--- frontend/src/pages/watchlist.js
+++ frontend/src/pages/watchlist.js
@@ -1,45 +1,53 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import Cookies from "universal-cookie";
 import axios from "axios";
 const cookies = new Cookies();
 
 const Watchlist = (props) => {
-  console.log(props);
+  const [watchlist, setWatchlist] = useState([]);
   const user_id = cookies.get("user_id");
 
-  if (user_id) {
+  useEffect(() => {
     const getWatchlist = async () => {
-      console.log('getWatchlist');
-      console.log('user_id', user_id);
+      console.log('getWatchlist is getting the list for user_id', user_id);
       try {
         const response = await axios.get(`http://localhost:3001/watchlists/${user_id}`);
         console.log(response.data);
+        setWatchlist(response.data);
       } catch (error) {
         console.error("Error getting watchlist:", error);
       }
+    };
+
+    if (user_id) {
+      getWatchlist();
     }
+  }, [user_id]);
 
-    getWatchlist();
-    return (
-      <div>
-        <h1>Watchlist</h1>
-        <p>Here are the stocks you are watching</p>
-        <ul>
- 
-        </ul>
-      </div>
-    );
-  }
-  else {
-    return (
-      <div>
-        <h1>Watchlist</h1>
+  return (
+    <div>
+      <h1>Watchlist</h1>
+      {user_id ? (
+        <div>
+          <p>Here are the stocks you are watching:</p>
+          <ul>
+            {watchlist.map((item, index) => (
+              <li key={index}>
+                {Object.entries(item).map(([key, value]) => (
+                  <p key={key}>
+                    <strong>{key}:</strong> {value}
+                  </p>
+                ))}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
         <p>Please log in to view your watchlist</p>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 };
-
-
 
 export default Watchlist;
