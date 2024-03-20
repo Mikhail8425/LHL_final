74de9c38f2a7ddabc10230efe854e078e0832d98
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-20 11:44:39 -0400

    minor formatting

--- frontend/src/pages/watchlist.js
+++ frontend/src/pages/watchlist.js
@@ -1,10 +1,7 @@
-
-// Filename - pages/blogs.js
- 
 import React from "react";
- 
+
 const Watchlist = () => {
-    return <h1>You can write your blogs!</h1>;
+  return <h1>You can write your blogs!</h1>;
 };
- 
+
 export default Watchlist;
