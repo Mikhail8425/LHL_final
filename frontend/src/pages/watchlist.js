1059b7f5e127341e3f9d9b34812271ff21bcd1ff
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-20 12:09:30 -0400

    Watchlist grabs the cookie

--- frontend/src/pages/watchlist.js
+++ frontend/src/pages/watchlist.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
-const Watchlist = () => {
-  return <h1>You can write your blogs!</h1>;
+const Watchlist = (props) => {
+  console.log(props);
+  const user_id = cookies.get("user_id");
+  if (user_id) {
+    return (
+      <div>
+        <h1>Watchlist</h1>
+        <p>Here are the stocks you are watching</p>
+        <ul>
+ 
+        </ul>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
+        <h1>Watchlist</h1>
+        <p>Please log in to view your watchlist</p>
+      </div>
+    );
+  }
 };
+
+
 
 export default Watchlist;
