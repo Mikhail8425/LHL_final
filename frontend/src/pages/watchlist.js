424545d494f8c02116041f1133835001df73281c
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-20 12:23:03 -0400

    watchlist grabs the list

--- frontend/src/pages/watchlist.js
+++ frontend/src/pages/watchlist.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Cookies from "universal-cookie";
+import axios from "axios";
 const cookies = new Cookies();
 
 const Watchlist = (props) => {
   console.log(props);
   const user_id = cookies.get("user_id");
+
   if (user_id) {
+    const getWatchlist = async () => {
+      console.log('getWatchlist');
+      console.log('user_id', user_id);
+      try {
+        const response = await axios.get(`http://localhost:3001/watchlists/${user_id}`);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error getting watchlist:", error);
+      }
+    }
+
+    getWatchlist();
     return (
       <div>
         <h1>Watchlist</h1>
