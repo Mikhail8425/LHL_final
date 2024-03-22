adfbbd65eb022ade0854ad6dd85af4f40398de06
Eric <ericbradshaw98@gmail.com>
2024-03-22 18:31:01 -0400

    modified css

--- frontend/src/components/component/StockListDetailsItem.jsx
+++ frontend/src/components/component/StockListDetailsItem.jsx
@@ -39,7 +39,7 @@ const StockListDetailsItem = ({ tickerCurrent }) => {
 
   const { data2, loading2, error2 } = useChart(`${tickerCurrent}/range/${multiplier}/${timespan}/${start}/${end}?adjusted=true&sort=asc&limit=120&`);
 
-  
+
 
   const handleMultiplierChange = (event) => {
     setMultiplier(event.target.value);
@@ -63,16 +63,16 @@ const StockListDetailsItem = ({ tickerCurrent }) => {
 
   useEffect(() => {
     if (!data2 || !data2.results || !Array.isArray(data2.results)) return;
-  
+
     const formattedData = data2.results.map(item => {
       return [item.t, [item.o, item.h, item.l, item.c]];
     });
-  
+
     setSeries([{ data: formattedData }]);
   }, [data2]);
-  
+
   if (loading || loading2 || loading3) return <div>Loading...</div>; // Check if any data is loading
-  
+
   if (error || error2 || error3) {
     return (
       <div>
@@ -80,7 +80,7 @@ const StockListDetailsItem = ({ tickerCurrent }) => {
       </div>
     );
   }
-  
+
   // Check if data.results is undefined or empty
   if (!data || !data.results || data.results.length === 0) {
     return <div>No results found</div>;
@@ -98,46 +98,56 @@ const StockListDetailsItem = ({ tickerCurrent }) => {
 
     <div className="stock-item">
       <div className='stock-container'>
-        <div className='stock-name'>
-          <h3>
-            {companyInfo.name && <li>{companyInfo.name} ({companyInfo.ticker})</li>} 
-          </h3>
-        </div>
+
         <div className="mixed-chart">
           <div>
-          <div className='stock-chart'>
-            <Chart
-              options={options}
-              series={series}
-              type="candlestick"
-              width="700"
-            /> 
-          </div>
-          <div className="controls">
-            <label>Multiplier:</label>
-            <input type="text" value={multiplier} onChange={handleMultiplierChange} />
+            <div className='stock-chart'>
+              <Chart
+                options={options}
+                series={series}
+                type="candlestick"
+                width="700"
+              />
+            </div>
+            <div className="controls">
+              <label>Multiplier:</label>
+              <input type="text" value={multiplier} onChange={handleMultiplierChange} />
 
-            <label>Timespan:</label>
-            <select value={timespan} onChange={handleTimespanChange}>
-              <option value="hour">Hour</option>
-              <option value="day">Day</option>
-              <option value="week">Week</option>
-              <option value="month">Month</option>
-              <option value="quarter">Quarter</option>
-              <option value="year">Year</option>
-            </select>
+              <label>Timespan:</label>
+              <select value={timespan} onChange={handleTimespanChange}>
+                <option value="hour">Hour</option>
+                <option value="day">Day</option>
+                <option value="week">Week</option>
+                <option value="month">Month</option>
+                <option value="quarter">Quarter</option>
+                <option value="year">Year</option>
+              </select>
 
-            <label>Start:</label>
-            <input type="date" value={start} onChange={handleStartChange} />
+              <label>Start:</label>
+              <input type="date" value={start} onChange={handleStartChange} />
 
-            <label>End:</label>
-            <input type="date" value={end} onChange={handleEndChange} />
-          </div>
+              <label>End:</label>
+              <input type="date" value={end} onChange={handleEndChange} />
+            </div>
 
           </div>
 
           <div className='company-details'>
             <li>
+              {companyInfo.name &&
+                <li style={{ display: 'inline' }}>
+                {companyInfo.name && `${companyInfo.name} (${companyInfo.ticker})`}
+                {companyInfo.branding.logo_url &&
+                  <img
+                    src={companyInfo.branding.logo_url + '?apiKey=5zppMBtonCBY1SJ42kijFfL2V7co5_MN'}
+                    alt="Logo"
+                    style={{ width: '100px', height: 'auto', marginLeft: '10px' }}
+                  />
+                }
+              </li>
+              
+              }
+
               {companyInfo.market && <li><strong>Market:</strong> {companyInfo.market}</li>}
               {companyInfo.locale && <li><strong>Locale:</strong> {companyInfo.locale}</li>}
               {companyInfo.currency_name && <li><strong>Currency Name:</strong> {companyInfo.currency_name}</li>}
@@ -151,84 +161,84 @@ const StockListDetailsItem = ({ tickerCurrent }) => {
               {companyInfo.sic_code && <li><strong>SIC Code:</strong> {companyInfo.sic_code}</li>}
               {companyInfo.sic_description && <li><strong>SIC Description:</strong> {companyInfo.sic_description}</li>}
               {companyInfo.list_date && <li><strong>List Date:</strong> {companyInfo.list_date}</li>}
-              {companyInfo.branding.logo_url && <li><strong>Logo: </strong><img src={companyInfo.branding.logo_url + '?apiKey=5zppMBtonCBY1SJ42kijFfL2V7co5_MN'} alt="Logo" style={{ width: '100px', height: 'auto' }} /></li>}
+
 
             </li>
           </div>
         </div>
       </div>
-      <div className='company-info-financials'> 
-      <div className='company-info-container'>
-        <div className="company-info">
-          <div className='company-title'>
-            {companyInfo.branding.logo_url && <li><img src={companyInfo.branding.logo_url + '?apiKey=5zppMBtonCBY1SJ42kijFfL2V7co5_MN'} alt="Logo" style={{ width: '100px', height: 'auto' }} /></li>}
-          </div>
-          <li>
-            {companyInfo.description && <li>{companyInfo.description}</li>}
-            <div className='company-links'>
-              {companyInfo.phone_number && <li><strong>Phone Number:</strong> {companyInfo.phone_number}</li>}
-              {companyInfo.address && <li><strong>Address:</strong> {companyInfo.address.address1}, {companyInfo.address.city}, {companyInfo.address.state} {companyInfo.address.postal_code}</li>}
-              {companyInfo.homepage_url && <li><strong>Homepage URL:</strong> <a href={companyInfo.homepage_url}>{companyInfo.homepage_url}</a></li>}
-              {companyInfo.total_employees && <li><strong>Total Employees:</strong> {companyInfo.total_employees}</li>}
+      <div className='company-info-financials'>
+        <div className='company-info-container'>
+          <div className="company-info">
+            <div className='company-title'>
+              {companyInfo.branding.logo_url && <li><img src={companyInfo.branding.logo_url + '?apiKey=5zppMBtonCBY1SJ42kijFfL2V7co5_MN'} alt="Logo" style={{ width: '100px', height: 'auto' }} /></li>}
             </div>
-          </li>
+            <li>
+              {companyInfo.description && <li>{companyInfo.description}</li>}
+              <div className='company-links'>
+                {companyInfo.phone_number && <li><strong>Phone Number:</strong> {companyInfo.phone_number}</li>}
+                {companyInfo.address && <li><strong>Address:</strong> {companyInfo.address.address1}, {companyInfo.address.city}, {companyInfo.address.state} {companyInfo.address.postal_code}</li>}
+                {companyInfo.homepage_url && <li><strong>Homepage URL:</strong> <a href={companyInfo.homepage_url}>{companyInfo.homepage_url}</a></li>}
+                {companyInfo.total_employees && <li><strong>Total Employees:</strong> {companyInfo.total_employees}</li>}
+              </div>
+            </li>
+          </div>
+        </div>
+
+
+        <div className='financials-container'>
+          <div className="financials">
+            <h3>Financial Information</h3>
+            <select value={selectedItemId} onChange={handleChange}>
+              <option value="">Select Item</option>
+              {data3 && data3.results && data3.results.map((item, index) => (
+                <option key={index} value={item.id}>{item.id}</option>
+              ))}
+            </select>
+
+            <select value={selectedFinancialCategory} onChange={handleChangeFinancialCategory}>
+              <option value="">Select Financial Category</option>
+              {selectedItemId && data3 && data3.results && data3.results.find(item => item.id === selectedItemId) &&
+                Object.keys(data3.results.find(item => item.id === selectedItemId).financials || {}).map(category => (
+                  Object.keys(data3.results.find(item => item.id === selectedItemId).financials[category]).map((subCategory, index) => (
+                    <option key={index} value={`${category}.${subCategory}`}>{data3.results.find(item => item.id === selectedItemId).financials[category][subCategory].label}</option>
+                  ))
+                ))
+              }
+            </select>
+
+            <ul>
+              {selectedItemId && data3 && data3.results && data3.results.map((item, index) => (
+                item.id === selectedItemId && (
+                  <li key={index}>
+                    <div className='finance-list-item'>
+                      <strong>ID: </strong> {item.id}<br />
+                      <strong>Start Date: </strong> {item.start_date}<br />
+                      <strong>End Date: </strong> {item.end_date}<br />
+
+
+                      {selectedFinancialCategory && item.financials[selectedFinancialCategory.split('.')[0]] && item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]] && (
+                        <>
+                          <strong>{selectedFinancialCategory.replace('_', ' ').replace('.', ': ')}: </strong>
+                          {item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]].unit === 'USD' &&
+                            `$`}
+                          {item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]].value}
+                          {item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]].unit && ` ${item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]].unit}`}
+                          <br /><br />
+                        </>
+                      )}
+                    </div>
+                    {/* Add other properties as needed */}
+                  </li>
+                )
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-
-
-  <div className='financials-container'>
-  <div className="financials">
-    <h3>Financial Information</h3>
-    <select value={selectedItemId} onChange={handleChange}>
-      <option value="">Select Item</option>
-     {data3 && data3.results && data3.results.map((item, index) => (
-      <option key={index} value={item.id}>{item.id}</option>
-      ))}
-    </select>
-
-    <select value={selectedFinancialCategory} onChange={handleChangeFinancialCategory}>
-      <option value="">Select Financial Category</option>
-      {selectedItemId && data3 && data3.results && data3.results.find(item => item.id === selectedItemId) &&
-        Object.keys(data3.results.find(item => item.id === selectedItemId).financials || {}).map(category => (
-          Object.keys(data3.results.find(item => item.id === selectedItemId).financials[category]).map((subCategory, index) => (
-          <option key={index} value={`${category}.${subCategory}`}>{data3.results.find(item => item.id === selectedItemId).financials[category][subCategory].label}</option>
-          ))
-       ))
-    }
-    </select>
-
-  <ul>
-    {selectedItemId && data3 && data3.results && data3.results.map((item, index) => (
-      item.id === selectedItemId && (
-        <li key={index}>
-          <div className='finance-list-item'>
-            <strong>ID: </strong> {item.id}<br />
-            <strong>Start Date: </strong> {item.start_date}<br />
-            <strong>End Date: </strong> {item.end_date}<br />
-
-
-            {selectedFinancialCategory && item.financials[selectedFinancialCategory.split('.')[0]] && item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]] && (
-              <>
-                <strong>{selectedFinancialCategory.replace('_', ' ').replace('.', ': ')}: </strong>
-                {item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]].unit === 'USD' &&
-                  `$`}
-                {item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]].value}
-                {item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]].unit && ` ${item.financials[selectedFinancialCategory.split('.')[0]][selectedFinancialCategory.split('.')[1]].unit}`}
-                <br /><br />
-              </>
-            )}
-          </div>
-          {/* Add other properties as needed */}
-        </li>
-      )
-    ))}
-  </ul>
-</div>
-    </div>
-    </div>
     </div>
   );
-  
+
 };
 StockListDetailsItem.propTypes = {
   tickerCurrent: PropTypes.string.isRequired,
