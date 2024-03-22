4c930a748e0665c1c5dc4d22dc0082ad3ec3227a
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-22 16:02:11 -0400

    display two plans

--- frontend/src/pages/plans.js
+++ frontend/src/pages/plans.js
@@ -1,9 +1,37 @@
 import React from "react";
+import "../styles/plans.scss";
 
 const Plans = () => {
+  const plans = [
+    {
+      id: 1,
+      name: 'Monthly Plan',
+      price: 19.99,
+      billingCycle: 'month',
+      description: 'This is a great plan for getting started.'
+    },
+    {
+      id: 2,
+      name: 'Yearly Plan',
+      price: 199.99,
+      billingCycle: 'year',
+      description: 'This is a great plan for saving money.'
+    }
+  ];
+
   return (
-    <div className="body-wrapper">
-      <h1>Stock Market Magnet Plans</h1>
+    <div className="body-wrapper subscription-plans-container" >
+      <h2>Subscription Plans</h2>
+      <ul>
+        {plans.map(plan => (
+          <li key={plan.id} >
+            <h3>{plan.name}</h3>
+            <p>${plan.price.toFixed(2)} / {plan.billingCycle}</p>
+            <p>{plan.description}</p>
+            {/* You can add more details here, like features included in each plan */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
