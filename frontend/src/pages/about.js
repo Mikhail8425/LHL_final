38180c2fa959aa216a66a4786ac29eb0ea6ccf24
Libin Ali <ali.libin@yahoo.com>
2024-03-25 10:35:40 -0400

    Added styling to about us page

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,28 +1,34 @@
 import React from "react";
+import FacebookIcon from "../components/icon/Facebookicon";
+import TwitterIcon from "../components/icon/Twittericon";
+import AboutUsImage from "../assets/about-us.jpeg"
+import "../styles/aboutus.scss";
 
 const About = () => {
   return (
-    <div className="body-wrapper">
-      <h1>About Stock Market Magnet</h1>
-      <p>Welcome to Stock Market Magnet, your go-to app for all things stocks! Whether you're a seasoned investor or just getting started, Stock Market Magnet has you covered with comprehensive information on stocks, financials, pricing, and more.</p>
-      <h2>Key Features:</h2>
-      <ul>
-        <li>Access detailed information about stocks</li>
-        <li>View financial data and pricing trends</li>
-        <li>Create and manage your personalized watchlist</li>
-
-      </ul>
-      <h2>Our Mission:</h2>
-      <p>At Stock Market Magnet, our mission is to empower investors by providing them with the tools and insights they need to make informed decisions in the dynamic world of stock trading.</p>
-      <h2>Contact Us:</h2>
-      <p>If you have any questions, feedback, or suggestions, please feel free to contact us at <a href="mailto:contact@stockmarketmagnet.com">contact@stockmarketmagnet.com</a>. We'd love to hear from you!</p>
-      <h2>Follow Us:</h2>
-      <p>Stay updated with the latest news and updates by following us on social media:</p>
-      <ul>
-        <li><a href="https://twitter.com/StockMarketMagnet" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-        <li><a href="https://facebook.com/StockMarketMagnet" target="_blank" rel="noopener noreferrer">Facebook</a></li>
-
-      </ul>
+    <div>
+      <div className="about-wrapper">
+        <div className="about-us-image">
+          <img src={AboutUsImage} alt ="About Stock Market Magnet"/>
+        </div>
+        <div className="about-us-info">
+          <h2>About Stock Market Magnet</h2>
+          <p>Welcome to Stock Market Magnet, your go-to app for all things stocks! Whether you're a seasoned investor or just getting started, Stock Market Magnet has you covered with comprehensive information on stocks, financials, pricing, and more.</p>
+          <h3>Key Features</h3>
+            <p>&rarr; Access detailed information about stocks</p>
+            <p>&rarr; View financial data and pricing trends</p>
+            <p>&rarr; Create and manage your personalized watchlist</p>
+          <h3>Our Mission</h3>
+          <p>At Stock Market Magnet, our mission is to empower investors by providing them with the tools and insights they need to make informed decisions in the dynamic world of stock trading.</p>
+        </div>
+      </div>
+      <div className="about-socials">
+        <h3>Follow Us</h3>
+        <div className="about-socials-icon">
+            <p><a href="https://twitter.com/StockMarketMagnet" target="_blank" rel="noopener noreferrer"><FacebookIcon /></a></p>
+            <p><a href="https://facebook.com/StockMarketMagnet" target="_blank" rel="noopener noreferrer"><TwitterIcon /></a></p>
+          </div>
+        </div>
     </div>
   );
 };
